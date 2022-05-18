924a262e6259cd6b7677bc512c962e06c61632f5
Abu Sufian <m.asufian@siderian.com>
2022-05-19 03:12:17 +0600

    initial commit

---
+++ Identity Server/Configs/Config.cs
@@ -0,0 +1,36 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+
+namespace Identity_Server.Configs
+{
+    public class Config
+    {
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client()
+                {
+                    ClientId = "movieClient",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "movieAPI" }
+                }
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("movieAPI", "Movie API")
+            };
+
+        public static IEnumerable<TestUser> TestUsers =>
+            new TestUser[]
+            {
+                new TestUser()
+            };
+    }
+}
